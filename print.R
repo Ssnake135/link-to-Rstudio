ecd64f89365cc22c8da9efd418fe966e37b7ebfd
Ssnake135 <set.sanchez94@gmail.com>
2020-10-30 12:23:56 -0600

    Alo

--- print.R
+++ print.R
@@ -1,3 +1,10 @@
 print("This file was created within Rstudio")
 
 print("And now it lives on GitHub")
+
+
+'En el cuadro de git abrir el script click derecho y darle en diff
+hacer los comentarios y ponerle en commit para despues darle al push'
+
+
+
